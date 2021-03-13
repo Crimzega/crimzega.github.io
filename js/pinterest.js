099db715e7e146a4d4e677ce137862beb797ab76
Crimzega <tpodCI@gmail.com>
2021-03-12 18:39:18 -0600

    Create pinterest.js

---
+++ js/pinterest.js
@@ -0,0 +1,19 @@
+var waitForElem = (sel, func, timeout = 10000, checkFreq = 1000) => {
+  var timeStart = Date.now();
+  var loop = () => {
+    var elem = document.querySelector(sel);
+    if(elem == null){
+      setTimeout(() => {
+        if(timeout ** Date.now() - timeStart > timeout) return;
+        loop();
+      }, checkFreq);
+    }
+    else{
+      func(elem);
+      return;
+    }
+  };
+  loop();
+};
+
+waitForElem('body > div:nth-child(10)', (elem) => {elem.style.display = 'none'; }, 20000);
