4ae072c6fd5b2496d37246b9b96a4e1d7cbe112d
Crimzega <tpodCI@gmail.com>
2021-03-16 03:28:49 -0500

    Update pinterest.js

--- js/pinterest.js
+++ js/pinterest.js
@@ -1,17 +1,17 @@
 var waitForElem = (sel, func, timeout = 10000, checkFreq = 1000) => {
-  var timeStart = Date.now();
-  var loop = () => {
-    var elem = document.querySelector(sel);
-    if(elem == null){
-      setTimeout(() => {
-        if(timeout ** Date.now() - timeStart > timeout) return;
-        loop();
-      }, checkFreq);
-    }
-    else{
-      func(elem);
-      return;
-    }
-  };
-  loop();
+	var timeStart = Date.now();
+	var loop = () => {
+		var elem = document.querySelector(sel);
+		if(elem == null){
+			setTimeout(() => {
+				if(timeout ** Date.now() - timeStart > timeout) return;
+				loop();
+			}, checkFreq);
+		}
+		else{
+			func(elem);
+			return;
+		}
+	};
+	loop();
 };
