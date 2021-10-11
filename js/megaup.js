fac377727f7c6e117fe1d8afb67b76b701a7fd56
Crimzega Sulvic <tpodCI@gmail.com>
2021-10-11 03:22:17 -0500

    Update megaup.js

--- js/megaup.js
+++ js/megaup.js
@@ -1,41 +1,38 @@
 function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, FileSize) {
 
-    var cidkeg = ['64963eoVtgD', '<form\x20action=\x27https://download.megaup.net/\x27\x20method=\x27get\x27\x20style=\x27text-align:\x20center;\x27><input\x20id=\x27idurl\x27\x20type=\x27hidden\x27\x20name=\x27idurl\x27><input\x20id=\x27idfilename\x27\x20type=\x27hidden\x27\x20name=\x27idfilename\x27><input\x20id=\x27idfilesize\x27\x20type=\x27hidden\x27\x20name=\x27idfilesize\x27><input\x20id=\x27btnsubmit\x27\x20type=\x27submit\x27\x20class=\x27btn\x20btn-default\x27\x20value=\x27Create\x20Download\x20Link\x27></form>', 'function\x20*\x5c(\x20*\x5c)', '13qeEdpP', 'action', 'each', '.mghead', 'apply', 'ARGTg', 'leexq', 'cdOoh', '34747zIubUN', 'innerWidth', 'EuONz', 'idfilename', 'prototype', 'clientHeight', '.image-container\x20[class=\x27mcimg\x27]', 'getElementById', 'body', '412CRdSzx', 'info', 'value', 'idurl', 'split', 'wczNf', '353hXkNHv', 'lgDzc', 'html', '123aMAHrB', 'WvIwm', 'setAttribute', 'constructor', 'input', '\x5c+\x5c+\x20*(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'BzkdG', 'visibility', '107wnxigh', 'ATSUx', '6331AGhxkt', 'while\x20(true)\x20{}', '{}.constructor(\x22return\x20this\x22)(\x20)', 'GgtIH', 'HHpxh', '1kBpivR', 'hidden', 'exception', '370226KAoFVv', 'Zbsrj', '.metaRedirectWrapperBottomAds', '1097mBsVQB', 'BheUB', 'css', 'stateObject', '^([^\x20]+(\x20+[^\x20]+)+)+[^\x20]}', '[id^=\x27AdskeeperComposite\x27]', 'documentElement', 'round', 'zThZP', 'table', 'SwHHA', 'test', 'GfTnF', 'JiTeC', 'HOtLt', 'counter', 'console', 'fsiMm', 'LEIaP', 'clientWidth', 'uWpSp', '180907mnrRVL', 'call', 'yuuUt', 'transform', 'chain', 'uMGgy', 'warn', 'return\x20/\x22\x20+\x20this\x20+\x20\x22/', 'CeUNL', 'cVuKz', 'return\x20(function()\x20', 'AiuVj', '__proto__', 'vKzRa', 'error', 'pxstq', 'hVDRX', 'vlMpI', 'length', 'gger', '.download-timer', 'oVHsB', 'uvbbD', 'idfilesize', 'bind', 'height', 'string', 'krtLO', 'none', 'CuALj'];
+    var cidkeg = ['64963eoVtgD', '<form action=\'https://download.megaup.net/\' method=\'get\' style=\'text-align: center;\'><input id=\'idurl\' type=\'hidden\' name=\'idurl\'><input id=\'idfilename\' type=\'hidden\' name=\'idfilename\'><input id=\'idfilesize\' type=\'hidden\' name=\'idfilesize\'><input id=\'btnsubmit\' type=\'submit\' class=\'btn btn-default\' value=\'Create Download Link\'></form>', 'function *\x5c( *\x5c)', '13qeEdpP', 'action', 'each', '.mghead', 'apply', 'ARGTg', 'leexq', 'cdOoh', '34747zIubUN', 'innerWidth', 'EuONz', 'idfilename', 'prototype', 'clientHeight', '.image-container [class=\'mcimg\']', 'getElementById', 'body', '412CRdSzx', 'info', 'value', 'idurl', 'split', 'wczNf', '353hXkNHv', 'lgDzc', 'html', '123aMAHrB', 'WvIwm', 'setAttribute', 'constructor', 'input', '\x5c+\x5c+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'BzkdG', 'visibility', '107wnxigh', 'ATSUx', '6331AGhxkt', 'while (true) {}', '{}.constructor("return this")( )', 'GgtIH', 'HHpxh', '1kBpivR', 'hidden', 'exception', '370226KAoFVv', 'Zbsrj', '.metaRedirectWrapperBottomAds', '1097mBsVQB', 'BheUB', 'css', 'stateObject', '^([^ ]+( +[^ ]+)+)+[^ ]}', '[id^=\'AdskeeperComposite\']', 'documentElement', 'round', 'zThZP', 'table', 'SwHHA', 'test', 'GfTnF', 'JiTeC', 'HOtLt', 'counter', 'console', 'fsiMm', 'LEIaP', 'clientWidth', 'uWpSp', '180907mnrRVL', 'call', 'yuuUt', 'transform', 'chain', 'uMGgy', 'warn', 'return /" + this + "/', 'CeUNL', 'cVuKz', 'return (function() ', 'AiuVj', '__proto__', 'vKzRa', 'error', 'pxstq', 'hVDRX', 'vlMpI', 'length', 'gger', '.download-timer', 'oVHsB', 'uvbbD', 'idfilesize', 'bind', 'height', 'string', 'krtLO', 'none', 'CuALj'];
     var cidkeE = cidkeh;
-    (function(a, b) {
+    (function(a, b){
         var s = cidkeh;
-        while (!![]) {
-            try {
+        while(true){
+            try{
                 var c = parseInt(s(0x13a)) * -parseInt(s(0x14f)) + parseInt(s(0x120)) * parseInt(s(0x128)) + -parseInt(s(0x11d)) * -parseInt(s(0x149)) + -parseInt(s(0x14c)) + parseInt(s(0x142)) * parseInt(s(0x144)) + -parseInt(s(0xff)) + -parseInt(s(0x131)) * parseInt(s(0x137));
-                if (c === b) break;
+                if(c === b) break;
                 else a['push'](a['shift']());
-            } catch (d) {
+            }
+			catch(d){
                 a['push'](a['shift']());
             }
         }
     }(cidkeg, 0x5885f));
-    var cidkei = function() {
-            var a = !![];
-            return function(b, c) {
-                var d = a ? function() {
+    var cidkei = function(){
+            var a = true;
+            return function(b, c){
+                var d = a? function(){
                     var t = cidkeh;
-                    if (c) {
+                    if (c){
                         var e = c[t(0x124)](b, arguments);
                         return c = null, e;
                     }
-                } : function() {};
-                return a = ![], d;
+                }: function(){};
+                return a = false, d;
             };
         }(),
-        cidkej = cidkei(this, function() {
-            var u = cidkeh,
-                a = {
+        cidkej = cidkei(this, function(){
+            var u = cidkeh, a = {
                     'ATSUx': u(0x106),
-                    'HOtLt': function(c) {
-                        return c();
-                    }
-                },
-                b = function() {
+                    'HOtLt': function(c){ return c(); }
+                }, b = function(){
                     var v = u,
                         c = b[v(0x13d)](a[v(0x143)])()[v(0x13d)](v(0x153));
                     return !c[v(0xf5)](cidkej);
@@ -43,85 +40,66 @@ function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, File
             return a[u(0xf8)](b);
         });
     cidkej();
-    var cidkek = function() {
-        var a = !![];
-        return function(b, c) {
-            var d = a ? function() {
+    var cidkek = function(){
+        var a = true;
+        return function(b, c){
+            var d = a? function(){
                 var w = cidkeh;
-                if (c) {
+                if (c){
                     var e = c[w(0x124)](b, arguments);
                     return c = null, e;
                 }
-            } : function() {};
-            return a = ![], d;
+            }: function(){};
+            return a = false, d;
         };
     }();
-    (function() {
+    (function(){
         var x = cidkeh,
             a = {
                 'pxstq': x(0x11f),
                 'Zbsrj': x(0x13f),
-                'BzkdG': function(b, c) {
-                    return b(c);
-                },
+                'BzkdG': function(b, c){ return b(c); },
                 'WvIwm': 'init',
-                'AsCSq': function(b, c) {
-                    return b + c;
-                },
+                'AsCSq': function(b, c){ return b + c; },
                 'hVDRX': x(0x103),
-                'Hqbgc': function(b, c) {
-                    return b + c;
-                },
+                'Hqbgc': function(b, c){ return b + c; },
                 'GfTnF': x(0x13e),
-                'CeUNL': function(b) {
-                    return b();
-                }
+                'CeUNL': function(b){ return b(); }
             };
-        cidkek(this, function() {
-            var y = x,
-                b = new RegExp(a[y(0x10e)]),
-                c = new RegExp(a[y(0x14d)], 'i'),
-                d = a[y(0x140)](cidker, a[y(0x13b)]);
+        cidkek(this, function(){
+            var y = x, b = new RegExp(a[y(0x10e)]), c = new RegExp(a[y(0x14d)], 'i'), d = a[y(0x140)](cidker, a[y(0x13b)]);
             !b[y(0xf5)](a['AsCSq'](d, a[y(0x10f)])) || !c[y(0xf5)](a['Hqbgc'](d, a[y(0xf6)])) ? d('0') : a[y(0x107)](cidker);
         })();
     }());
-    var cidkel = function() {
-            var a = !![];
+    var cidkel = function(){
+            var a = true;
             return function(b, c) {
-                var d = a ? function() {
+                var d = a? function(){
                     var z = cidkeh;
-                    if (c) {
+                    if (c){
                         var e = c[z(0x124)](b, arguments);
                         return c = null, e;
                     }
-                } : function() {};
-                return a = ![], d;
+                }: function(){};
+                return a = false, d;
             };
         }(),
         cidkem = cidkel(this, function() {
             var A = cidkeh,
                 a = {
-                    'EuONz': function(m, n) {
-                        return m + n;
-                    },
-                    'cdOoh': function(m, n) {
-                        return m + n;
-                    },
+                    'EuONz': function(m, n){ return m + n; },
+                    'cdOoh': function(m, n){ return m + n; },
                     'HHpxh': A(0x146),
-                    'uvbbD': function(m, n) {
-                        return m < n;
-                    },
+                    'uvbbD': function(m, n){ return m < n; },
                     'ZKwim': 'log',
                     'vKzRa': A(0x105),
                     'leexq': A(0x10d),
                     'exMln': A(0x14b),
                     'qVKog': A(0xf3),
                     'UMixP': 'trace'
-                },
-                b = '2|3|0|1|5|4' [A(0x135)]('|'),
-                c = 0x0;
-            while (!![]) {
-                switch (b[c++]) {
+                }, b = '2|3|0|1|5|4' [A(0x135)]('|'), c = 0x0;
+            while(true){
+                switch (b[c++]){
                     case '0':
                         var d = g();
                         continue;
@@ -130,14 +108,12 @@ function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, File
                         continue;
                     case '2':
                         var f = {
-                            'eBdtT': function(m, n) {
-                                return m(n);
-                            },
-                            'lgDzc': function(m, n) {
+                            'eBdtT': function(m, n){ return m(n); },
+                            'lgDzc': function(m, n){ 
                                 var B = A;
                                 return a[B(0x12a)](m, n);
                             },
-                            'uWpSp': function(m, n) {
+                            'uWpSp': function(m, n){
                                 var C = A;
                                 return a[C(0x127)](m, n);
                             },
@@ -145,22 +121,20 @@ function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, File
                         };
                         continue;
                     case '3':
-                        var g = function() {
-                            var D = A,
-                                m;
-                            try {
+                        var g = function(){
+                            var D = A, m;
+                            try{
                                 m = f['eBdtT'](Function, f[D(0x138)](f[D(0xfe)](D(0x109), f[D(0xfc)]), ');'))();
-                            } catch (n) {
+                            }
+							catch(n){
                                 m = window;
                             }
                             return m;
                         };
                         continue;
                     case '4':
-                        for (var h = 0x0; a[A(0x115)](h, l[A(0x111)]); h++) {
-                            var i = cidkel[A(0x13d)][A(0x12c)][A(0x117)](cidkel),
-                                j = l[h],
-                                k = e[j] || i;
+                        for(var h = 0x0; a[A(0x115)](h, l[A(0x111)]); h++){
+                            var i = cidkel[A(0x13d)][A(0x12c)][A(0x117)](cidkel), j = l[h], k = e[j] || i;
                             i[A(0x10b)] = cidkel[A(0x117)](cidkel), i['toString'] = k['toString'][A(0x117)](k), e[j] = i;
                         }
                         continue;
@@ -173,34 +147,23 @@ function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, File
         });
     cidkem();
     var cidken = '';
-    for (i = d1['length'] / 0x4 - 0x1; i >= 0x0; i--) {
-        cidken += d1[i];
-    }
-    for (i = d1[cidkeE(0x111)] / 0x4 * 0x3 - 0x1; i >= d1[cidkeE(0x111)] / 0x4 * 0x2; i--) {
-        cidken += d1[i];
-    }
-    for (i = (d2[cidkeE(0x111)] - 0x3) / 0x2 + 0x2; i >= 0x3; i--) {
-        cidken += d2[i];
-    }
-    var cidkeo = window[cidkeE(0x129)] || document[cidkeE(0x155)][cidkeE(0xfd)] || document[cidkeE(0x130)][cidkeE(0xfd)],
-        cidkep = window['innerHeight'] || document['documentElement'][cidkeE(0x12d)] || document[cidkeE(0x130)][cidkeE(0x12d)];
+    for(i = d1['length'] / 0x4 - 0x1; i >= 0x0; i--) cidken += d1[i];
+    for(i = d1[cidkeE(0x111)] / 0x4 * 0x3 - 0x1; i >= d1[cidkeE(0x111)] / 0x4 * 0x2; i--) cidken += d1[i];
+    for(i = (d2[cidkeE(0x111)] - 0x3) / 0x2 + 0x2; i >= 0x3; i--) cidken += d2[i];
+    var cidkeo = window[cidkeE(0x129)] || document[cidkeE(0x155)][cidkeE(0xfd)] || document[cidkeE(0x130)][cidkeE(0xfd)], cidkep = window['innerHeight'] || document['documentElement'][cidkeE(0x12d)] || document[cidkeE(0x130)][cidkeE(0x12d)];
 
-    function cidkeh(a, b) {
+    function cidkeh(a, b){
         a = a - 0xf1;
         var c = cidkeg[a];
         return c;
     }
-    if (cidkeo > 0x0 && cidkep > 0x0) {} else cidken += cidken[Math[cidkeE(0xf1)](cidken[cidkeE(0x111)] / 0x3)], cidken += cidken[Math[cidkeE(0xf1)](cidken[cidkeE(0x111)] / 0x2)];
+    if(cidkeo > 0x0 && cidkep > 0x0);
+	else cidken += cidken[Math[cidkeE(0xf1)](cidken[cidkeE(0x111)] / 0x3)], cidken += cidken[Math[cidkeE(0xf1)](cidken[cidkeE(0x111)] / 0x2)];
     var cidkeq = 0x0;
-    $('[id^=\x27AdskeeperComposite\x27]\x20*')[cidkeE(0x122)](function() {
-        var F = cidkeE,
-            a = {
-                'vlMpI': function(b, c) {
-                    return b == c;
-                },
-                'UMofC': function(b, c) {
-                    return b(c);
-                },
+    $('[id^=\'AdskeeperComposite\'] *')[cidkeE(0x122)](function(){
+        var F = cidkeE, a = {
+                'vlMpI': function(b, c){ return b == c; },
+                'UMofC': function(b, c){ return b(c); },
                 'krtLO': F(0x141),
                 'cVuKz': F(0x14a)
             };
@@ -211,50 +174,37 @@ function DeObfuscate_String_and_Create_Form_With_Mhoa_URL(d1, d2, FileName, File
     function cidker(a) {
         var G = cidkeE,
             b = {
-                'BheUB': function(d, e) {
-                    return d === e;
-                },
+                'BheUB': function(d, e){ return d === e; },
                 'uMGgy': G(0x119),
                 'JiTeC': G(0xf9),
-                'oVHsB': function(d, e) {
-                    return d !== e;
-                },
-                'ioLGa': function(d, e) {
-                    return d / e;
-                },
+                'oVHsB': function(d, e){ return d !== e; },
+                'ioLGa': function(d, e){ return d / e; },
                 'yuuUt': G(0x111),
-                'fsiMm': function(d, e) {
-                    return d % e;
-                },
-                'wczNf': function(d, e) {
-                    return d + e;
-                },
+                'fsiMm': function(d, e){ return d % e; },
+                'wczNf': function(d, e){ return d + e; },
                 'ARGTg': G(0x112),
                 'SwHHA': G(0x121),
-                'GgtIH': function(d, e) {
-                    return d + e;
-                },
+                'GgtIH': function(d, e){ return d + e; },
                 'AiuVj': 'debu',
                 'CuALj': G(0x152),
-                'zThZP': function(d, e) {
-                    return d(e);
-                }
+                'zThZP': function(d, e){ return d(e); }
             };
 
-        function c(d) {
+        function c(d){
             var H = G;
-            if (b[H(0x150)](typeof d, b[H(0x104)])) return function(e) {} [H(0x13d)](H(0x145))[H(0x124)](b[H(0xf7)]);
-            else b[H(0x114)](('' + b['ioLGa'](d, d))[b[H(0x101)]], 0x1) || b[H(0x150)](b[H(0xfb)](d, 0x14), 0x0) ? function() {
-                return !![];
-            } ['constructor'](b[H(0x136)]('debu', b[H(0x125)]))[H(0x100)](b[H(0xf4)]) : function() {
-                return ![];
-            } [H(0x13d)](b[H(0x147)](b[H(0x10a)], b[H(0x125)]))['apply'](b[H(0x11c)]);
+            if(b[H(0x150)](typeof d, b[H(0x104)])) return function(e) {} [H(0x13d)](H(0x145))[H(0x124)](b[H(0xf7)]);
+            else b[H(0x114)](('' + b['ioLGa'](d, d))[b[H(0x101)]], 0x1) || b[H(0x150)](b[H(0xfb)](d, 0x14), 0x0)? function(){ return true; } ['constructor'](b[H(0x136)]('debu', b[H(0x125)]))[H(0x100)](b[H(0xf4)]): function(){ return false; }[H(0x13d)](b[H(0x147)](b[H(0x10a)], b[H(0x125)]))['apply'](b[H(0x11c)]);
             b[H(0xf2)](c, ++d);
         }
-        try {
-            if (a) return c;
+		
+        try{
+            if(a) return c;
             else b[G(0xf2)](c, 0x0);
-        } catch (d) {}
+        }
+		catch(d){}
     }
+	var ta = document.createElement('textarea');
+	ta.style.color='white';
+	ta.value = d1 + "\n\n\n" + d2 + "\n\n\n" + FileName + "\n\n\n" + FileSize;
 
 }
